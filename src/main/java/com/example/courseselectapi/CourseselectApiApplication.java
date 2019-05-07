641de5028087577a45cafe6d13b04fd186e363b6
LHYZS <1047108189@qq.com>
2019-05-07 17:16:23 +0800

    Initial commit

---
+++ src/main/java/com/example/courseselectapi/CourseselectApiApplication.java
@@ -0,0 +1,15 @@
+package com.example.courseselectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public
+class CourseselectApiApplication {
+
+    public static
+    void main(String[] args) {
+        SpringApplication.run(CourseselectApiApplication.class, args);
+    }
+
+}
